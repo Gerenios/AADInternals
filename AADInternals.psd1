4fbf8d9b731ff76f87837d0d1082c49892bc1078
Nestori Syynimaa <nestori.syynimaa@gerenios.com>
2019-05-21 20:57:58 +0300

    v0.2.0

--- AADInternals.psd1
+++ AADInternals.psd1
@@ -4,7 +4,7 @@
 	RootModule = 'AADInternals.psm1'
 
 	# Version number of this module.
-	ModuleVersion = '0.1.9'
+	ModuleVersion = '0.2.0'
 
 	# Supported PSEditions
 	# CompatiblePSEditions = @()
--- AADSyncSettings.ps1
+++ AADSyncSettings.ps1
@@ -237,3 +237,141 @@ function Update-SyncCredentials
         }
     }
 }
+
+# May 17th 2019
+function Set-ADSyncAccountPassword
+{
+<#
+    .SYNOPSIS
+    Sets the password of ADSync service account
+
+    .Description
+    Sets the password of ADSync service account to AD and WID configuration database. MUST be run on AADConnect server
+    as company administrator.
+  
+    .Example
+    Set-AADIntADSyncAccountPassword -NewPassword 'Pa$$w0rd'
+    Password successfully updated to AD and configuration database!
+    Remember to restart the sync service: Restart-Service ADSync
+
+    Name                           Value
+    ----                           -----
+    ADDomain                       company.com  
+    ADUser                         MSOL_4bc4a34e95fa
+    ADUserPassword                 Pa$$w0rd
+    AADUser                        Sync_SRV01_4bc4a34e95fa@company.onmicrosoft.com
+    AADUserPassword                $.1%(lxZ&/kNZz[r
+
+    .Example
+    Set-AADIntADSyncAccountPassword -NewPassword 'Pa$$w0rd' -RestartADSyncService
+    Password successfully updated to AD and configuration database!
+    
+    Name                           Value
+    ----                           -----
+    ADDomain                       company.com  
+    ADUser                         MSOL_4bc4a34e95fa
+    ADUserPassword                 Pa$$w0rd
+    AADUser                        Sync_SRV01_4bc4a34e95fa@company.onmicrosoft.com
+    AADUserPassword                $.1%(lxZ&/kNZz[r
+
+    WARNING: Waiting for service 'Microsoft Azure AD Sync (ADSync)' to stop...
+    WARNING: Waiting for service 'Microsoft Azure AD Sync (ADSync)' to start...
+#>
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [String]$NewPassword,
+        [Switch]$RestartADSyncService
+     )
+    Process
+    {
+        # Check that we are on AADConnect server
+        if((Get-Service ADSync -ErrorAction SilentlyContinue) -eq $null)
+        {
+            Write-Error "This command needs to be run on a computer with AADConnect"
+            return
+        }
+
+        # Add the encryption reference (should always be there)
+        Add-Type -path 'C:\Program Files\Microsoft Azure AD Sync\Bin\mcrypt.dll’
+
+        # Get the current configuration
+        $SyncCreds = Get-SyncCredentials
+        $SyncUser = $SyncCreds.ADUser
+
+        Write-Verbose "Updating password for $SyncUser"
+
+        # Reset the account password in AD
+        try
+        {
+            Set-ADAccountPassword -Identity $SyncUser -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $NewPassword -Force)
+        }
+        catch
+        {
+            # There might be complexity etc. requirements
+            throw $_
+            return
+        }
+
+        # Escaping password for xml
+        $NewPassword = [System.Security.SecurityElement]::Escape($NewPassword)
+
+        # Create a new config
+        $ADDecryptedConfig=@"
+<encrypted-attributes>
+ <attribute name="Password">$NewPassword</attribute>
+</encrypted-attributes>
+"@
+        # Read the encrypt/decrypt key settings
+        $SQLclient = new-object System.Data.SqlClient.SqlConnection -ArgumentList "Data Source=(localdb)\.\ADSync;Initial Catalog=ADSync"
+        $SQLclient.Open()
+        $SQLcmd = $SQLclient.CreateCommand()
+        $SQLcmd.CommandText = "SELECT keyset_id, instance_id, entropy FROM mms_server_configuration"
+        $SQLreader = $SQLcmd.ExecuteReader()
+        $SQLreader.Read() | Out-Null
+        $key_id = $SQLreader.GetInt32(0)
+        $instance_id = $SQLreader.GetGuid(1)
+        $entropy = $SQLreader.GetGuid(2)
+        $SQLreader.Close()
+
+        # Load keys
+        $KeyMgr = New-Object -TypeName Microsoft.DirectoryServices.MetadirectoryServices.Cryptography.KeyManager
+        $KeyMgr.LoadKeySet($entropy, $instance_id, $key_id)
+        $key = $null
+        $KeyMgr.GetActiveCredentialKey([ref]$key)
+        $key2 = $null
+        $KeyMgr.GetKey(1, [ref]$key2)
+
+        # Encrypt
+        $ADCryptedConfig = $null
+        $key2.EncryptStringToBase64($ADDecryptedConfig,[ref]$ADCryptedConfig)
+
+        # Write the updated AA password
+        $SQLcmd = $SQLclient.CreateCommand()
+        $SQLcmd.CommandText = "UPDATE mms_management_agent SET encrypted_configuration=@pwd WHERE ma_type = 'AD'"
+        $SQLcmd.Parameters.AddWithValue("@pwd",$ADCryptedConfig) | Out-Null
+        $UpdatedRows = $SQLcmd.ExecuteNonQuery() 
+        $SQLclient.Close()
+        
+        if($UpdatedRows -ne 1)
+        {
+            Write-Error "Updated $UpdatedRows while should update 1. Could be error"
+            return
+        }
+
+        Write-Host "Password successfully updated to AD and configuration database!"
+
+        # Return        
+        Get-SyncCredentials
+
+        # Restart the ADSync service if requested
+        if($RestartADSyncService)
+        {
+            Restart-Service ADSync
+        }
+        else
+        {
+            Write-Host "Remember to restart the sync service: Restart-Service ADSync" -ForegroundColor Yellow
+        }
+    }
+}
