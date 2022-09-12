955a8b53e32c83539745ef1967f0a57a79ccad8a
Sapir Federovsky <sapirf@semperis.com>
2022-09-12 12:49:25 +0300

    test

--- SPO.ps1
+++ SPO.ps1
@@ -623,7 +623,6 @@ function Get-SPOTest
  
         $siteDomain=$Site.Split("/")[2]
         # Create a WebSession object
-        Write-Host $siteDomain
         $siteSession = Create-WebSession -SetCookieHeader $AuthHeader -Domain $siteDomain
         #$digest = Get-SPODigest -Site $Site
         # Set the headers
@@ -643,7 +642,7 @@ function Get-SPOTest
         $at = Get-AccessTokenForSPO -Tenant $Tenant -SaveToCache
         #$AccessToken = Get-AccessTokenFromCache -AccessToken $AccessToken -Resource "https://$Tenant.sharepoint.com/" -ClientId "9bc3ab49-b65d-410a-85ad-de819febfddc"
         $headers["Authorization"] = "Bearer $at"
-        $response=Invoke-WebRequest -UseBasicParsing -Uri "$Site" -Method Get -WebSession $siteSession  -Headers $headers  -ErrorAction SilentlyContinue 
+        $response=Invoke-WebRequest -UseBasicParsing -Uri "$Site/_api/contextinfo" -Method Get -WebSession $siteSession  -Headers $headers  -ErrorAction SilentlyContinue 
         #$response=Invoke-WebRequest -UseBasicParsing -Uri "$Site/_api/SP.Directory.DirectorySession/Group('18fec963-bea7-469e-a6d7-ab69aa7de58b')/Members/Add(objectId='00000000-0000-0000-0000-000000000000', principalName='testa%4054824v%2Eonmicrosoft%2Ecom')" -Method Post -WebSession $siteSession -ContentType "application/json;odata=verbose" -ErrorAction SilentlyContinue  -Headers $headers 
         <#if($response.count -gt 4)
         {
